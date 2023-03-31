94abee35caa5b41cda35c9e29f780ff077b7afb6
yqiuu <hpc_yqiuu@163.com>
2023-03-31 13:44:24 +0800

    feat: Add solver.py

--- chemistry/__init__.py
+++ chemistry/__init__.py
@@ -2,5 +2,6 @@ from .utils import *
 from .reaction import *
 from .surface_reaction import *
 from .rate import *
+from .solver import *
 from .equation import *
 from .meta_params import *
---
+++ chemistry/solver.py
@@ -0,0 +1,16 @@
+import torch
+from scipy.integrate import solve_ivp
+
+
+def solve_ode(target, t_span, y0, vectorized=True):
+    def wrapper(t_in, y_in):
+        t_in = torch.tensor(t_in, dtype=torch.float32)
+        y_in = torch.tensor(y_in, dtype=torch.float32)
+        if y_in.ndim == 2:
+            y_in = y_in.T
+        y_out = target(t_in, y_in)
+        if y_in.ndim == 2:
+            y_out = y_out.T
+        return y_out.numpy()
+
+    return solve_ivp(wrapper, t_span, y0, method='BDF', vectorized=vectorized)
