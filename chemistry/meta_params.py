d220328f19459f5d2bcf6ad88bef764853584482
yqiuu <hpc_yqiuu@163.com>
2023-04-03 10:41:14 +0800

    feat: Add more parameters

--- chemistry/meta_params.py
+++ chemistry/meta_params.py
@@ -3,4 +3,24 @@ from dataclasses import dataclass
 
 @dataclass
 class MetaParameters:
-    site_density: float = 1.5e15 # Site density on one grain [cm-2]
+    """Meta parameters.
+
+    Args:
+        T_dust_0 (float): Initial dust temperature [K].
+        site_density (float): Site density on one grain [cm^-2].
+
+        rate_cr_ion (float): Cosmic ray ionisation rate [s^-1].
+        rate_x_ion: (float): X-ray ionisation rate [s^-1].
+        rate_fe_ion: (float): Fe-ion-grain encounter [s^-1].
+        tau_cr_peak (float): Duration of peak grain temperature [s^-1].
+        T_cr_peak (float): Peak grain temperature due to cosmic ray heating [K].
+    """
+    # Grain parameters
+    T_dust_0: float = 10.
+    site_density: float = 1.5e15
+
+    rate_cr_ion: float = 1.3e-17
+    rate_x_ion: float = 0.
+    rate_fe_ion: float = 1e-14
+    tau_cr_peak: float = 1e-5
+    T_peak_cr: float = 70.
