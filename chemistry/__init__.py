24558f2e626806325d419102c930220a3f99f760
yqiuu <hpc_yqiuu@163.com>
2023-04-04 12:02:13 +0800

    feat: Add splines.py

--- chemistry/__init__.py
+++ chemistry/__init__.py
@@ -2,6 +2,7 @@ from .utils import *
 from .reaction import *
 from .surface_reaction import *
 from .rate import *
+from .splines import *
 from .solver import *
 from .equation import *
 from .meta_params import *
