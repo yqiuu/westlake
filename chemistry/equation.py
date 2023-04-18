79853338288267c0bb216aea4e276ff3caebdd54
yqiuu <hpc_yqiuu@163.com>
2023-04-18 17:39:42 +0800

    feat: Add create_problem

--- chemistry/equation.py
+++ chemistry/equation.py
@@ -1,5 +1,11 @@
+import numpy as np
 import torch
 from torch import nn
+
+from .utils import data_frame_to_key_tensor
+from .meta_params import MetaParameters
+from .reaction_matrices import create_reaction_data
+from .gas_reactions import create_gas_reactions_1st, create_gas_reactions_2nd
 
 
 class ReactionTerm(nn.Module):
@@ -29,3 +35,35 @@ class ReactionTerm(nn.Module):
             term_2 = y_in[:, self.inds_2r].prod(dim=-1)*self.rate_2(t_in)
             y_out.scatter_add_(1, inds_2p, term_2)
         return y_out
+
+
+def create_problem(df_reac, params_env, ab_0, ab_0_dtype='list'):
+    spec_table, rmat_1st, rmat_2nd = create_reaction_data(df_reac["reactants"], df_reac["products"])
+    formulae = df_reac["formula"].values.astype(str)
+    #
+    meta_params = MetaParameters()
+    # First order reactions
+    params_reac = data_frame_to_key_tensor(df_reac[["T_min", "T_max", "alpha", "beta", "gamma"]].iloc[rmat_1st.inds])
+    rate_1st = create_gas_reactions_1st(formulae[rmat_1st.inds], rmat_1st, params_env, params_reac, meta_params)
+    # Second order reactions
+    params_reac = data_frame_to_key_tensor(df_reac[["T_min", "T_max", "alpha", "beta", "gamma"]].iloc[rmat_2nd.inds])
+    rate_2nd = create_gas_reactions_2nd(formulae[rmat_2nd.inds], rmat_2nd, params_env, params_reac, meta_params)
+    # Initial condition
+    ab_0 = ab_0.copy()
+    ab_0_ = [0.]*len(spec_table)
+    for idx, spec in enumerate(spec_table.index):
+        if spec in ab_0:
+            ab_0_[idx] = ab_0.pop(spec)
+        elif spec == "GRAIN0":
+            ab_0_[idx] = meta_params.grain_ab_0
+    if len(ab_0) != 0:
+        raise ValueError("Invalid initial abundances.")
+
+    if ab_0_dtype == 'list':
+        pass
+    elif ab_0_dtype == 'numpy':
+        ab_0_ = np.asarray(ab_0_)
+    elif ab_0_dtype == 'torch':
+        ab_0_ = torch.tensor(ab_0_)
+
+    return ReactionTerm(rmat_1st, rate_1st, rmat_2nd, rate_2nd), ab_0_, spec_table
--- chemistry/meta_params.py
+++ chemistry/meta_params.py
@@ -1,13 +1,19 @@
+import math
 from dataclasses import dataclass
 
+from .constants import M_ATOM
 
-@dataclass
+
+@dataclass(frozen=True)
 class MetaParameters:
     """Meta parameters.
 
     Args:
         T_dust_0 (float): Initial dust temperature [K].
         site_density (float): Site density on one grain [cm^-2].
+        grain_density (float): Grain mass density [g cm^-3].
+        grain_radius (float): Grain radius [cm].
+        dtg_mass_ratio_0 (float): Initial dust to gas mass ratio.
 
         rate_cr_ion (float): Cosmic ray ionisation rate [s^-1].
         rate_x_ion: (float): X-ray ionisation rate [s^-1].
@@ -18,9 +24,22 @@ class MetaParameters:
     # Grain parameters
     T_dust_0: float = 10.
     site_density: float = 1.5e15
+    grain_density: float = 3.
+    grain_radius: float = 1e-5
+    dtg_mass_ratio_0: float = 1e-2
 
     rate_cr_ion: float = 1.3e-17
     rate_x_ion: float = 0.
     rate_fe_ion: float = 1e-14
     tau_cr_peak: float = 1e-5
     T_peak_cr: float = 70.
+
+    @property
+    def grain_mass(self):
+        """Grain mass [ma]."""
+        return 4*math.pi/3*self.grain_radius**3*self.grain_density/M_ATOM
+
+    @property
+    def grain_ab_0(self):
+        """Initial grain abundance."""
+        return self.dtg_mass_ratio_0/self.grain_mass
