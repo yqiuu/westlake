664cf6fe1c2f1363b7554b215bb3ac5e5461edc3
yqiuu <hpc_yqiuu@163.com>
2023-05-08 17:04:07 +0800

    feat: Add compute_rates_reac
    -Add some comments

--- chemistry/reaction_rates.py
+++ chemistry/reaction_rates.py
@@ -19,11 +19,13 @@ class FixedReactionRate(nn.Module):
 
 
 class FormulaDictReactionModule(nn.Module):
-    def __init__(self, formula_dict, inds_fm_dict, inds_k, rate_sign, module_env, params_reac):
+    def __init__(self, formula_dict, inds_fm_dict,
+                 inds_reac, inds_k, rate_sign, module_env, params_reac):
         super(FormulaDictReactionModule, self).__init__()
         for i_fm, inds in enumerate(inds_fm_dict.values()):
             setattr(self, f"_params_reac_{i_fm}", params_reac.indexing(inds))
         self.formula_list = nn.ModuleList([formula_dict[key] for key in inds_fm_dict])
+        self.register_buffer("inds_reac", torch.tensor(inds_reac))
         self.register_buffer("inds_k", torch.tensor(inds_k))
         self.register_buffer("rate_sign", torch.tensor(rate_sign, dtype=torch.get_default_dtype()))
         if isinstance(module_env, TensorDict) or isinstance(module_env, nn.Module):
@@ -33,6 +35,9 @@ class FormulaDictReactionModule(nn.Module):
 
     def forward(self, t_in):
         return self.compute_rates(t_in)[self.inds_k]*self.rate_sign
+
+    def compute_rates_reac(self, t_in):
+        return self.compute_rates(t_in)[self.inds_reac]
 
     def compute_rates(self, t_in):
         params_env = self.module_env(t_in)
@@ -57,18 +62,26 @@ class FormulaDictReactionModule(nn.Module):
 def create_formula_dict_reaction_module(df_reac, rmat, formula_dict, module_env, param_names):
     df_sub = df_reac.iloc[rmat.inds_id]
 
-    lookup_sub = pd.DataFrame(np.arange(len(df_sub)), index=df_sub.index, columns=["index_sub"])
+    # The code below construct the following variables.
+    #   1. inds_reac, index in the reaction dataframe for the outputs of the reaction module.
+    #   2. inds_k, index of the rates in the equation for the outputs of the reaction module. This
+    #   should align with rmat.rate_sign
 
+    # Link the indices in the df_sub to those in the reaction dataframe.
+    lookup_sub = pd.DataFrame(np.arange(len(df_sub)), index=df_sub.index, columns=["index_sub"])
+    # Link the indices in the outputs of the reaction module to those in the df_sub.
     inds_fm_dict = defaultdict(list)
     for i_fm, fm in enumerate(df_sub["formula"]):
         inds_fm_dict[fm].append(i_fm)
     inds_fm = np.asarray(sum(inds_fm_dict.values(), start=[]))
     lookup_fm = pd.DataFrame(np.arange(len(inds_fm)), index=inds_fm, columns=["index_fm"])
-
+    # Link the indices in the outputs of the reaction module to those in the reaction dataframe.
     lookup_sub["index_fm"] = lookup_fm.loc[lookup_sub["index_sub"], "index_fm"].values
+    #
+    inds_reac = lookup_sub.loc[rmat.inds_id, "index_fm"].values
     inds_k = lookup_sub.loc[rmat.inds_k, "index_fm"].values
 
     return FormulaDictReactionModule(
-        formula_dict, inds_fm_dict, inds_k, rmat.rate_sign,
+        formula_dict, inds_fm_dict, inds_reac, inds_k, rmat.rate_sign,
         module_env, data_frame_to_tensor_dict(df_sub[param_names])
     )
