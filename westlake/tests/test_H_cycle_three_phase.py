ffba0dbe340153aa1d3d9b2e121eb5b119af9a88
邱逸盛 <yishengq@zhejianglab.com>
2023-09-05 16:02:23 +0800

    test: Test the three phase model

---
+++ westlake/tests/test_H_cycle_three_phase.py
@@ -0,0 +1,54 @@
+import os
+import pickle
+
+import numpy as np
+from numpy.testing import assert_allclose
+import pandas as pd
+import torch
+import westlake
+
+from utils import get_abs_fname
+
+
+torch.set_default_dtype(torch.float64)
+FILE_NAME = "H_cycle_three_phase.pickle"
+ATOL = 1e-20
+
+
+def test_H_cycle():
+    res_new = solve_H_cycle()
+    res_fid = pickle.load(open(get_abs_fname(FILE_NAME), "rb"))
+    assert_allclose(res_fid.y, res_new.y, atol=ATOL)
+
+
+def solve_H_cycle():
+    fname = get_fname("H_cycle_three_phase.h5")
+    df_reac = pd.read_hdf(fname, key="reactions")
+    df_surf = pd.read_hdf(fname, key="surface_parameters")
+    df_spec = pd.read_hdf(fname, key="species")
+    df_act = None
+
+    meta_params = westlake.MetaParameters(atol=ATOL, ab_0_min=1e-40)
+    medium = westlake.StaticMedium({'Av': 10., "den_gas": 1e4, "T_gas": 17., "T_dust": 17.})
+    reaction_term = westlake.create_three_phase_model(
+        df_reac, df_surf, df_act, df_spec, medium, meta_params
+    )
+
+    ab_0 = westlake.dervie_initial_abundances({'H2': .5,}, df_spec, meta_params)
+    t_begin = 0.
+    t_end = 1e5
+    n_eval = 100
+    t_span = (t_begin, t_end)
+    t_eval = np.linspace(t_begin, t_end, n_eval)
+    res = westlake.solve_kinetic(reaction_term, t_span, ab_0, meta_params, t_eval=t_eval)
+    return res
+
+
+def get_fname(fname):
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), fname)
+
+
+if __name__ == "__main__":
+    # Save test data
+    res = solve_H_cycle()
+    pickle.dump(res, open(FILE_NAME, "wb"))
