34ce662a4ad57efe27b1e879be1ecd8b57607b7d
邱逸盛 <yishengq@zhejianglab.com>
2023-09-15 15:51:37 +0800

    fix: Fix some issues related to ConstantRateTerm

--- westlake/astrochem.py
+++ westlake/astrochem.py
@@ -37,8 +37,8 @@ def builtin_astrochem_reactions_2nd(meta_params):
     }
 
 
-def create_constant_rate_model(df_reac):
-    reaction_matrix = ReactionMatrix(df_reac)
+def create_constant_rate_model(df_reac, df_spec=None, den_norm=None):
+    reaction_matrix = ReactionMatrix(df_reac, df_spec)
     rmat_1st, rmat_2nd = reaction_matrix.create_index_matrices()
     rate_1st = ConstantReactionRate(rmat_1st, df_reac["rate"].values)
     rate_2nd = ConstantReactionRate(rmat_2nd, df_reac["rate"].values)
@@ -46,7 +46,7 @@ def create_constant_rate_model(df_reac):
     # unnecessary.
     rmat_1st.rate_sign = None
     rmat_2nd.rate_sign = None
-    return ConstantRateTerm(rmat_1st, rate_1st, rmat_2nd, rate_2nd)
+    return ConstantRateTerm(rmat_1st, rate_1st, rmat_2nd, rate_2nd, den_norm)
 
 
 def create_two_phase_model(df_reac, df_spec, df_surf, medium, meta_params,
--- westlake/reaction_terms.py
+++ westlake/reaction_terms.py
@@ -13,28 +13,31 @@ from .assemblers import Assembler
 
 
 class ConstantRateTerm(nn.Module):
-    def __init__(self, rmat_1st, rmod_1st, rmat_2nd, rmod_2nd):
+    def __init__(self, rmat_1st, rmod_1st, rmat_2nd, rmod_2nd, den_norm=None):
         super(ConstantRateTerm, self).__init__()
         self.register_reactions("1st", rmat_1st, rmod_1st)
         self.register_reactions("2nd", rmat_2nd, rmod_2nd)
         self.inds_id_1st = rmat_1st.inds_id_uni
         self.inds_id_2nd = rmat_2nd.inds_id_uni
+        if den_norm is None:
+            den_norm = torch.ones(1)
+        else:
+            den_norm = torch.tensor(float(den_norm))
+        self.register_buffer("den_norm", den_norm)
 
     def register_reactions(self, postfix, rmat, rmod):
         setattr(self, f"asm_{postfix}", Assembler(rmat))
         setattr(self, f"rmod_{postfix}", rmod)
 
     def forward(self, t_in, y_in):
-        den_norm = 1.
         rates_1st, rates_2nd = self.compute_rates()
-        return self.asm_1st(y_in, rates_1st, den_norm) \
-            + self.asm_2nd(y_in, rates_2nd, den_norm)
+        return self.asm_1st(y_in, rates_1st, self.den_norm) \
+            + self.asm_2nd(y_in, rates_2nd, self.den_norm)
 
     def jacobian(self, t_in, y_in):
-        den_norm = 1.
         rates_1st, rates_2nd = self.compute_rates()
-        return self.asm_1st.jacobain(y_in, rates_1st, den_norm) \
-            + self.asm_2nd.jacobain(y_in, rates_2nd, den_norm)
+        return self.asm_1st.jacobain(y_in, rates_1st, self.den_norm) \
+            + self.asm_2nd.jacobain(y_in, rates_2nd, self.den_norm)
 
     def compute_rates(self):
         rates_1st = self.rmod_1st()
