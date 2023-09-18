9d83b8543f143765052dd052522723a9713cdb86
邱逸盛 <yishengq@zhejianglab.com>
2023-09-18 10:54:12 +0800

    refactor: Remove unused codes

--- westlake/reaction_terms.py
+++ westlake/reaction_terms.py
@@ -185,43 +185,6 @@ class ThreePhaseTerm(nn.Module):
         return rates
 
 
-@dataclass(frozen=True)
-class AstrochemProblem:
-    spec_table: pd.DataFrame
-    rmat_1st: ReactionMatrix
-    rmat_2nd: ReactionMatrix
-    reaction_term: nn.Module
-    ab_0: torch.Tensor # Initial abundances
-
-    def __repr__(self):
-        return "Attributes: spec_table, rmat_1st, rmat_2nd, reaction_term, ab_0"
-
-
-def create_astrochem_problem(df_reac, params_med, ab_0, spec_table_base=None, ab_0_min=0.):
-    meta_params = MetaParameters()
-    #
-    spec_table, rmat_1st, rmat_2nd = create_reaction_data(
-        df_reac["reactant_1"], df_reac["reactant_2"], df_reac["products"], spec_table_base)
-    formulae = df_reac["formula"].values.astype(str)
-    # First order reactions
-    params_reac = data_frame_to_tensor_dict(
-        df_reac[["is_unique", "T_min", "T_max", "alpha", "beta", "gamma"]].iloc[rmat_1st.inds],
-    )
-    rate_1st = create_gas_reaction_module_1st(
-        formulae[rmat_1st.inds], rmat_1st, params_med, params_reac, meta_params)
-    # Second order reactions
-    params_reac = data_frame_to_tensor_dict(
-        df_reac[["is_unique", "T_min", "T_max", "alpha", "beta", "gamma"]].iloc[rmat_2nd.inds])
-    rate_2nd = create_gas_reaction_module_2nd(
-        formulae[rmat_2nd.inds], rmat_2nd, params_med, params_reac, meta_params)
-    #
-    reaction_term = ReactionTerm(rmat_1st, rate_1st, rmat_2nd, rate_2nd)
-    #
-    ab_0_ = dervie_initial_abundances(ab_0, spec_table, meta_params, ab_0_min)
-
-    return AstrochemProblem(spec_table, rmat_1st, rmat_2nd, reaction_term, ab_0_)
-
-
 def dervie_initial_abundances(ab_0_dict, spec_table, meta_params):
     """Derive the initial abundances of grains and electrons.
 
@@ -250,20 +213,3 @@ def dervie_initial_abundances(ab_0_dict, spec_table, meta_params):
     ab_0[spec_table.loc["e-", "index"]] = np.sum(spec_table["charge"].values*ab_0)
 
     return ab_0
-
-
-def compute_reaction_rates(problem, t_0=None):
-    """Compute the reaction rates that match the input data frame."""
-    if t_0 is None:
-        t_0 = torch.tensor([0.])
-    with torch.no_grad():
-        inds_uni_1, to_uni_1 = np.unique(problem.rmat_1st.inds, return_index=True)
-        rates_1 = problem.reaction_term.rate_1(t_0)[to_uni_1].numpy()
-        inds_uni_2, to_uni_2 = np.unique(problem.rmat_2nd.inds, return_index=True)
-        rates_2 = problem.reaction_term.rate_2(t_0)[to_uni_2].numpy()
-    n_rate = max(max(inds_uni_1), max(inds_uni_2)) + 1
-    # TODO: Consider the cases for multiple times.
-    rates = np.empty(n_rate, dtype=np.float32)
-    rates[inds_uni_1] = rates_1
-    rates[inds_uni_2] = rates_2
-    return rates
