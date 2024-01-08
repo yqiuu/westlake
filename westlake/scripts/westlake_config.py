0617dbed7271e77b5ba83209d07b1506f6bb8894
邱逸盛 <yishengq@zhejianglab.com>
2024-01-08 16:02:10 +0800

    feat: Allow to set directory of the config file

--- westlake/scripts/westlake_config.py
+++ westlake/scripts/westlake_config.py
@@ -1,11 +1,17 @@
 import shutil
 from pathlib import Path
+from argparse import ArgumentParser
 
 
 def main():
-    dirname = Path(__file__).parent.parent
-    fname = dirname/Path("config")/Path("config_template.yml")
-    shutil.copy(fname, "./config.yml")
+    parser = ArgumentParser()
+    parser.add_argument(
+        '--dirname', type=str, default='./', help='Directory to save the config file.')
+    args = parser.parse_args()
+    fname_target = Path(args.dirname)/Path("config.yml")
+
+    fname = Path(__file__).parent.parent/Path("config")/Path("config_template.yml")
+    shutil.copy(fname, fname_target)
 
 
 if __name__ == "__main__":
