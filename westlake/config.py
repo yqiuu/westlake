c56db7b142d2babccad59dd377e1184cd24adbe8
邱逸盛 <yishengq@zhejianglab.com>
2024-01-09 16:39:00 +0800

    feat: New method to config.yml

--- westlake/config.py
+++ westlake/config.py
@@ -1,9 +1,11 @@
 import math
 from dataclasses import dataclass
+from pathlib import Path
 
 from astropy import units
+from docstring_parser import parse
 
-from ..constants import M_ATOM
+from .constants import M_ATOM
 
 
 @dataclass(frozen=True)
@@ -11,27 +13,30 @@ class Config:
     """Config.
 
     Args:
-        model (str):
+        model (str): Model name.
             - 'simple': Gas phase model.
             - 'two phase': Two phase model.
             - 'three phase': Three phase model.
+        t_start (float): Starting time.
+        t_end(float): Ending time.
+        save_rate_coeffs (bool): Switch to save rate coefficents.
 
         site_density (float): Site density on one grain [cm^-2].
         grain_density (float): Grain mass density [g cm^-3].
         grain_radius (float): Grain radius [cm].
-        dtg_mass_ratio (float): Initial dust to gas mass ratio.
-        diffusion_barrier_thickness (float): Diffusion_barrier_thickness [cm].
+        dtg_mass_ratio (float): Dust-to-gas mass ratio.
+        diffusion_barrier_thickness (float): Diffusion barrier thickness [cm].
         chemical_barrier_thickness (float): Grain reaction activation energy
             barrier width [cm].
-        zeta_cr (float): Cosmic ray ionisation rate [s^-1].
-        zeta_xr: (float): X-ray ionisation rate [s^-1].
-        zeta_fe: (float): Fe-ion-grain encounter [s^-1].
-        tau_cr_peak (float): Duration of peak grain temperature [s^-1].
+        factor_cr_peak (float): Prefactor to compute comsmic ray
+            evaporation [s^-1].
         T_grain_cr_peak (float): Peak grain temperature due to cosmic ray
             heating [K].
-        sticking_coeff_neutral (float):
-        sticking_coeff_positive (float):
-        sticking_coeff_negative (float):
+        sticking_coeff_neutral (float): Sticking coefficient of neutral species.
+        sticking_coeff_positive (float): Sticking coefficient of positive
+            species.
+        sticking_coeff_negative (float): Sticking coefficient of negative
+            species.
         vib_to_dissip_freq_ratio (float): The ratio of the surface-molecule bond
             frequency to the frequency at which energy is lost to the grain
             surface (Garrod el al. 2007).
@@ -41,29 +46,34 @@ class Config:
             to diffusion energy for mantle species.
         num_active_layers (float): Number of active layers.
         uv_flux (float): UV flux.
-
-        use_photodesorption (bool): If True, enable photodesorption.
+        use_competition (bool): Switch of competition mechanism
 
         den_Av_ratio (float): Density to Av ratio to compute self-shielding.
-        H2_shielding (str): Set "Lee+1996" to turn on H2 shielding. Set None to
-            turn off.
-        CO_shielding (str): Set "Lee+1996" to turn on CO shielding. Set None to
-            turn off.
+        H2_shielding (str): Switch of H2 sheilding (None, Lee+1996).
+        CO_shielding (str): Switch of CO shielding (None, Lee+1996).
 
-        method (str): ODE solver.
+        Av (float): Visual extinction.
+        den_gas (float): Gas density.
+        T_gas (float): Gas temperature.
+        T_dust (float): Dust temperature.
+        zeta_cr (float): Cosmic ray ionisation rate [s^-1].
+        zeta_xr (float): X-ray ionisation rate [s^-1].
+
+        solver (str): ODE solver.
         rtol (float): Relative tolerance.
         atol (float): Ababsolute tolerance.
         ab_0_min (float): Minimum initial abundances.
         use_auto_jac (bool): If True, use `jacrev` in `torch` to compute
-            jacobian.
-
+            jacobian, which is slow but accurate.
         to_second (float): A unit factor that converts the desired unit to
             second. The default value converts year to second.
-
         special_species (tuple): Species whose abundance is irrelevant in the
             simulation, e.g. CR, CRP and Photon.
     """
-    model: str
+    model: str = "two phase"
+    t_start: float = 0.
+    t_end: float = 1.e+6
+    save_rate_coeffs: bool = True
 
     # Grain parameters
     site_density: float = 1.5e15
@@ -72,8 +82,6 @@ class Config:
     dtg_mass_ratio: float = 1e-2
     diffusion_barrier_thickness: float = 1e-8
     chemical_barrier_thickness: float = 1e-8
-    zeta_cr: float = 1.3e-17
-    zeta_xr: float = 0.
     factor_cr_peak: float = 3e-19
     T_grain_cr_peak: float = 70.
     sticking_coeff_neutral: float = 1.
@@ -98,18 +106,15 @@ class Config:
     den_gas: float = 2.e+4
     T_gas: float = 10.
     T_dust: float = 10
+    zeta_cr: float = 1.3e-17
+    zeta_xr: float = 0.
 
     # Numerics
-    t_start: float = 0.
-    t_end: float = 1.e+6
     solver: str = "BDF"
     rtol: float = 1e-4
     atol: float = 1e-20
     ab_0_min: float = 0.
     use_auto_jac: bool = False
-
-    #
-    save_rate_coeffs: bool = True
 
     #
     to_second: float = units.year.to(units.second)
@@ -144,3 +149,44 @@ def fixed_dtg_mass_ratio(ab_He, dtg_mass_ratio=None):
     if dtg_mass_ratio is None:
         dtg_mass_ratio = Config.dtg_mass_ratio
     return dtg_mass_ratio*(1 + 4*ab_He)
+
+
+def save_config_template(fname):
+    def create_description(text):
+        text = text.replace("None", "null")
+        text = text.replace("\n", " ")
+        text = text.split(" ")
+        text_ret = "# "
+        len_curr = 0
+        for word in text:
+            len_new = len_curr + len(word) + 1
+            if len_new > 80 or word.startswith("-"):
+                text_ret += "\n# "
+                len_curr = 0
+            text_ret += f"{word} "
+            len_curr += len(word) + 1
+        text_ret += "\n"
+        return text_ret
+
+    exclude_list = [
+        "to_second",
+        "special_species",
+    ]
+    with open(fname, "w") as fp:
+        for var in parse(Config.__doc__).params:
+            if var.arg_name in exclude_list:
+                continue
+
+            fp.write(create_description(var.description))
+            val = getattr(Config, var.arg_name)
+            if val is None:
+                text = "{}: null".format(var.arg_name, val)
+            elif var.type_name == "float":
+                if abs(val) >= 1e-3 and abs(val) < 1e4:
+                    text = "{}: {:.2f}".format(var.arg_name, val)
+                else:
+                    text = "{}: {:.2e}".format(var.arg_name, val)
+            else:
+                text = "{}: {}".format(var.arg_name, val)
+            text += "\n\n"
+            fp.write(text)
--- westlake/scripts/westlake_config.py
+++ westlake/scripts/westlake_config.py
@@ -2,16 +2,15 @@ import shutil
 from pathlib import Path
 from argparse import ArgumentParser
 
+from ..config import save_config_template
+
 
 def main():
     parser = ArgumentParser()
     parser.add_argument(
         '--dirname', type=str, default='./', help='Directory to save the config file.')
     args = parser.parse_args()
-    fname_target = Path(args.dirname)/Path("config.yml")
-
-    fname = Path(__file__).parent.parent/Path("config")/Path("config_template.yml")
-    shutil.copy(fname, fname_target)
+    save_config_template(Path(args.dirname)/Path("config.yml"))
 
 
 if __name__ == "__main__":
